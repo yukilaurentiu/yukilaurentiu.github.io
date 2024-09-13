8bcb8c335c05af2e76958e0594429bd94543a158
yuki <yuki.laurentiu@gmail.com>
2024-09-13 11:56:42 +0900

    trying out forming

--- app/src/components/ContactForm.js
+++ app/src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import emailjs from "@emailjs/browser";
+import './Contact.css';
 
 function ContactForm() {
   // Function to handle form submission
@@ -25,25 +26,50 @@ function ContactForm() {
   };
 
   return (
-    <div>
-      <div>
+    <div className="contact-container">
+      <div className="header-container">
         <h1>Get in touch 👋</h1>
         <div className="App-p">
           <p>Feel free to send me a message.</p>
-          <p> I would love to hear from you!</p>
+          <p>I would love to hear from you!</p>
         </div>
       </div>
       <form onSubmit={handleSubmit} className="App-form">
-        <label>Name</label>
-        <input type="text" name="name" placeholder="Your Name" required />
-        <label>Email</label>
-        <input type="email" name="email" placeholder="Your Email" required />
-        <label>Message</label>
-        <textarea name="message" placeholder="Your Message" required></textarea>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="Your Name" required />
+        
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Your Email" required />
+        
+        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message" placeholder="Your Message" required></textarea>
+        
         <button type="submit">Send</button>
       </form>
     </div>
   );
+  
+
+  // return (
+  //   <div>
+  //     <div>
+  //       <h1>Get in touch 👋</h1>
+  //       <div className="App-p">
+  //         <p>Feel free to send me a message.</p>
+  //         <p> I would love to hear from you!</p>
+  //       </div>
+  //     </div>
+  //     <form onSubmit={handleSubmit} className="App-form">
+  //       <label>Name</label>
+  //       <input type="text" name="name" placeholder="Your Name" required />
+  //       <label>Email</label>
+  //       <input type="email" name="email" placeholder="Your Email" required />
+  //       <label>Message</label>
+  //       <textarea name="message" placeholder="Your Message" required></textarea>
+  //       <button type="submit">Send</button>
+  //     </form>
+  //   </div>
+  // );
 }
 
 export default ContactForm;
