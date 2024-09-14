9cbea7a1c00cda72a7ed6f894c5c5044ca16eda0
yuki <yuki.laurentiu@gmail.com>
2024-09-15 08:57:03 +0900

    create routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ContactForm from './components/ContactForm';
 import Example from './components/Example';
+import BusinessCard from './components/BusinessCard';
 
 function App() {
   return (
-    <div className="App">
-      <card className="App-card">
-        <ContactForm/>
-      </card>
-      <Example/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Example />} />
+          <Route path="/business-card" element={<BusinessCard />} />
+          <Route path="/contact" element={<ContactForm />} />
+        </Routes>
+      </div>
+    </Router>
+    // <div className="App">
+    //   <card className="App-card">
+    //     <ContactForm/>
+    //   </card>
+    //   <Example/>
+    // </div>
   );
 }
 
---
+++ app/src/components/BusinessCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BusinessCard() {
+
+  return (
+   <div>
+    BusinessCard
+   </div>
+  );
+}
+
+export default BusinessCard;
