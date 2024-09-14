c591f92cb2453303cb46c5a36bd61bff74b153c0
yuki <yuki.laurentiu@gmail.com>
2024-09-14 18:53:33 +0900

    created example.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import ContactForm from './components/ContactForm';
+import Example from './components/Example';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <card className="App-card">
         <ContactForm/>
       </card>
+      <Example/>
     </div>
   );
 }
---
+++ app/src/components/Example.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Example() {
+
+  return (
+   <div>
+    Example
+   </div>
+  );
+}
+
+export default Example;
